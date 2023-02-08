817f69c5c3e82a1a7cc294ad3c4d8edeac487cf6
v74c63t <vanesst5@uci.edu>
2023-02-07 23:03:12 -0800

    Added comments to tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -1,38 +1,37 @@
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-# import pdb
 
+# we get a set of stopwords from nltk.corpus which will be used to help 
+# filter out any stop words in the words list
 stop_words = set(stopwords.words('english'))
 
-# def printFreq(frequenciesDict):
-#     iterator = iter(frequenciesDict.items())
-#     for i in range(50):
-#         print(next(iterator))
-
 def tokenizeCount(words, freq):
-    # this function takes in a response and a dictionary and parses through
-    # the content of the response and counts the frequecy that each word appears
-    # given that it is not a stop word
-    # word_freq: defaultdict(int)
-    # we are assuming that wDict is a defaultdictionary of integers
-    #if resp.raw_response != None:
-    #words = word_tokenize(text)
+    '''
+    words: a list of words obtained from using word_tokenize on text of the page
+    freq: a defaultdict(int) {key: word, value: word count} that will be updated with the frequency of each word
+
+    this function goes through the words list and updates the count in the dictionary for the word
+    as long as it is a valid word
+
+    it returns back the updated dictionary
+    '''
+
+    # we go through and add 1 to the count for each word as long as the word is 
+    # valid meaning it is not a stopword and it is alphanumeric
     for word in words:
         if word not in set(stopwords.words('english')) and word.isalnum():
-            # if the word is not a stop word and it is alphanumeric
-            # we will turn the word into lower case and add 1 to the count 
-            # in the defaultdictionary for said word
             freq[word] += 1
     return freq
-    # sorting dictionaries dont rly work/do anything
-    # wDict is a defaultdict so this might cause problems so ill just return the updated dict
-    # return dict(sorted(wDict.items(), key = lambda x:(x[1] * -1, x[0])))
 
 def remove_stop_words(words):
-    #words = word_tokenize(resp_text.lower())
+    '''
+    words: a list of words obtained from using word_tokenize on text of the page
+    we return a list of words that is filtered to not contain any stop words (and words that are not alphanumeric)
+    '''
     filter = []
     for word in words:
+        # words that are stopwords or are not alphanumeric are added to the filter list
         if word not in set(stopwords.words('english')) and word.isalnum():
             filter.append(word)
     return filter
